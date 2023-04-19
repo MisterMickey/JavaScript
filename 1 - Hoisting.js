c9700ec41dd85273c7d212fb786aa20c14c85341
Shubham Singodiya <93720428+MisterMickey@users.noreply.github.com>
2023-04-20 01:03:44 +0530

    Create 1 - Hoisting.js

---
+++ 1 - Hoisting.js
@@ -0,0 +1,24 @@
+/*
+> Hoisting : During the creation phase in Execution Context, code is scanned for variable and function declarations and a new property is added to the 
+           variable environment object. This property contains the variables and function and assigns them with a value(variable value will be set to 
+           'undefined' & for function, the value is function body itself).          
+           
+> Var variables and function declarations are hoisted.
+
+> Let and const are not hoisted( technically they are hoisted but their value is uninitialized and hence dumped into TDZ(Temporal Dead Zone)).
+> Function expressions and Arrow functions also depends if declared with var,let or const.
+
+
+*/
+
+console.log(hoistedVariable)//undefined
+hoistedFunction();//I'm Hoisted!
+console.log(notHoisted1)//ERROR!
+console.log(notHoisted2)//ERROR!
+
+
+var hoistedVariable = 5;
+function hoistedFunction(){ console.log("I'm Hoisted!");}
+
+let notHoisted1 = 1;
+const notHoisted2 = 1;
